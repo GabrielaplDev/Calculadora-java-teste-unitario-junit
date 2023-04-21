391ffbafc6e5b693dd0a766fd2687617a0a7a20f
GabrielaplDev <90151540+GabrielaplDev@users.noreply.github.com>
2023-04-21 12:59:54 -0300

    Teste unitario

---
+++ calculadoraJava-TesteUnitario-Junit/src/main/java/Calculadora.java
@@ -0,0 +1,19 @@
+
+public class Calculadora {
+
+		public double Soma( double num1, double num2) {
+			return num1 + num2;
+		}
+
+		public double Subtracao(double num1, double num2) {
+			return num1 - num2;
+		}
+		
+		public double Divisao(double num1, double num2) {
+			return num1/num2;
+		}
+		public double Potenciacao(double num1, double num2) {
+			return Math.pow(num1, num2);
+		}
+
+}
---
+++ calculadoraJava-TesteUnitario-Junit/src/test/java/CalculadoraTest.java
@@ -0,0 +1,54 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class CalculadoraTest {
+
+	@Test
+	 public void testeSoma() {
+		//Arrange
+		Calculadora calculadora = new Calculadora();
+		
+		//Act
+		double resultado = calculadora.Soma(10,10);
+		
+		//Assert
+		Assertions.assertEquals(resultado, calculadora.Soma(resultado, 0));
+	}
+	
+	public void testeSubtracao() {
+		//Arrange
+		Calculadora calculadora = new Calculadora();
+		
+		//ACT
+		double result = calculadora.Subtracao(20, 30);
+		
+		//Assert
+		Assertions.assertEquals(result, calculadora.Subtracao(result, result));
+	}
+	
+	public void testeDivisao() {
+		//Arrange
+				Calculadora calculadora = new Calculadora();
+				
+				//ACT
+				double result = calculadora.Divisao(20, 30);
+				
+				//Assert
+				Assertions.assertEquals(result, calculadora.Divisao(result, result));
+	}
+	
+	public void testePotencia() {
+		//Arrange
+				Calculadora calculadora = new Calculadora();
+				
+				//ACT
+				double result = calculadora.Potenciacao(20, 30);
+				
+				//Assert
+				Assertions.assertEquals(result, calculadora.Potenciacao(result, result));
+	}
+	
+
+}
